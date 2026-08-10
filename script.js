b610596991e08b5e873c71fd1db954e382b6fa87
Max Mashack <mmashack125@gmail.com>
2026-08-09 21:20:16 -0400

    files for portfolio website

---
+++ script.js
@@ -0,0 +1,9 @@
+//makes the button actually do something
+// this line is finding the button we made in the html
+const button = document.querySelector("button");
+
+// and this line is doing something with the button
+// (that is, making it create a pop-up with our text)
+function buttonClick() {
+  alert("You clicked the button! 🎉");
+}
